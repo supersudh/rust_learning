485e0e4c2bee28ba6bfaa2c6ee25fbf453a59d5f
Sudharsan Ravikumar <supersudh@gmail.com>
2024-04-26 23:05:17 +0100

    Variables in Rust with a JS Twist

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-mod print; // [supersudh] mod and the name of the file goes here, this is an import from our neighbouring file
+// mod print; // [supersudh] mod and the name of the file goes here, this is an import from our neighbouring file
+mod variables;
 
 fn main() {
     // println!("Hello, world!"); // [supersudh] deprecated
-    print::run()
+    // print::run()
+    variables::run();
 }
---
+++ src/variables.rs
@@ -0,0 +1,9 @@
+// Variables hold primitive data or references to data
+// Variables are immutable by default
+// Rust is a block-scoped language [supersudh] This would remind us of const, let, var confusions in JS :-P
+
+pub fn run() {
+    let name = "San Redux";
+
+    println!("My name is {}", name)
+}
