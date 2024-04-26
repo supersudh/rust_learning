f26984334238d91c354add772cce46bc3443e6a2
Sudharsan Ravikumar <supersudh@gmail.com>
2024-04-26 22:49:16 +0100

    String interpolation in Rust

--- src/print.rs
+++ src/print.rs
@@ -1,7 +1,9 @@
 pub fn run() { // public function
     println!("Hello from the print.rs file");
 
-    println!("Number: {}", 1); // Correct
+    // println!("Number: {}", 1); // Correct
 
     // WRONG println(1) -> leads to error
+
+    println!("{} is from {}", "Sudharsan", "UK");
 }
