fd16a8c4cd27a0f884a59fcd7a4b06193eafa534
Sudharsan Ravikumar <supersudh@gmail.com>
2024-04-26 22:55:43 +0100

    Positional Arguments, Named Arguments and Placeholder Traits

--- src/print.rs
+++ src/print.rs
@@ -1,9 +1,29 @@
-pub fn run() { // public function
+pub fn run() {
+    // public function
     println!("Hello from the print.rs file");
 
     // println!("Number: {}", 1); // Correct
 
     // WRONG println(1) -> leads to error
 
-    println!("{} is from {}", "Sudharsan", "UK");
+    println!("{} is from {}", "Sudharsan", "UK"); // String interpolation
+
+    // Positional Arguments
+    println!(
+        "{0} is from {1} and {0} likes to play{2}",
+        "Sudharsan", "UK", "chess"
+    );
+
+    // Named Arguments
+    println!(
+        "{name} likes to play {activity}",
+        name = "Sudharsan",
+        activity = "chess"
+    );
+
+    // Placeholder traits
+    // for Binary that trait is :b
+    // for hex trait is :x
+    // for octal trait is :o
+    println!("Binary {:b} Hex: {:x} Octal: {:o}", 10, 10, 10);
 }
