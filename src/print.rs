73cfd25e2e148699abf77d169a7e18f3e6b24d48
Sudharsan Ravikumar <supersudh@gmail.com>
2024-04-26 22:47:57 +0100

    printing integers in Rust

--- src/print.rs
+++ src/print.rs
@@ -1,3 +1,7 @@
 pub fn run() { // public function
-    println!("Hello from the print.rs file")
+    println!("Hello from the print.rs file");
+
+    println!("Number: {}", 1); // Correct
+
+    // WRONG println(1) -> leads to error
 }
