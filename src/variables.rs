69166e487e8a14a4dcf88f3190982967d6d4ad05
Sudharsan Ravikumar <supersudh@gmail.com>
2024-04-26 23:13:07 +0100

    [Constants and Multiple vars] 1. Ability to create and inspect constant variables 2. Ability to assign multiple vars

--- src/variables.rs
+++ src/variables.rs
@@ -8,4 +8,12 @@ pub fn run() {
     println!("My name is {} and I am {}", name, age); // [supersudh]sequence of code line is important here
     age = 100; // Reassign/mutate only for reuse in subsequent lines
     println!("My name is {} and I am {}", name, age); // Note to self: Sequence of code is important in Rust
+
+    // Define constant
+    const ID: i32 = 001; // Define a type
+    println!("ID: {}", ID);
+
+    // Assign multiple variables at once
+    let ( my_name, my_age ) = ("Sudharsan", 30);
+    println!("{} is {}", my_name, my_age);
 }
