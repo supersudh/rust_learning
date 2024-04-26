c08ff14a7fe4d00fd55a770f9068bc561e4bde30
Sudharsan Ravikumar <supersudh@gmail.com>
2024-04-26 23:22:51 +0100

    variable types

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 // mod print; // [supersudh] mod and the name of the file goes here, this is an import from our neighbouring file
-mod variables;
+// mod variables;
+mod types;
 
 fn main() {
     // println!("Hello, world!"); // [supersudh] deprecated
     // print::run()
-    variables::run();
+    // variables::run();
+    types::run();
 }
