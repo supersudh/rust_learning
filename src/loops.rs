f663be0178a4141ad80837db3090f0ad8fb367d9
Sudharsan Ravikumar <supersudh@gmail.com>
2024-04-28 11:15:19 +0100

    For Range loop

--- src/loops.rs
+++ src/loops.rs
@@ -1,5 +1,5 @@
 pub fn run() {
-    let mut count = 0;
+    // let mut count = 0;
 
     // ∞ loop
 
@@ -13,21 +13,34 @@ pub fn run() {
     // }
 
     // While loop (buzzer)
-    // const ZERO: i8 = 0;
-    // const THREE: i8 = 0;
-    // const FIVE: i8 = 5;
-    // const FIFTEEN: i8 = 15;
-    // const HUNDRED: i8 = 100;
-    while count <= 100 {
-        if count % 15 == 0 {
+    // const ZERO: i8 = 0; DOESN'T WORK WHY??? TODO
+    // const THREE: i8 = 0; DOESN'T WORK WHY??? TODO
+    // const FIVE: i8 = 5; DOESN'T WORK WHY??? TODO
+    // const FIFTEEN: i8 = 15; DOESN'T WORK WHY??? TODO
+    // const HUNDRED: i8 = 100; DOESN'T WORK WHY??? TODO
+    // while count <= 100 { // [START]WORKS
+    //     if count % 15 == 0 {
+    //         println!("fizzbuzz")
+    //     } else if count % 3 == 0 {
+    //         println!("fizz");
+    //     } else if count % 5 == 0 {
+    //         println!("buzz");
+    //     } else {
+    //         println!("{}", count)
+    //     }
+    //     count += 1
+    // } // [END] WORKS
+
+    // For Range
+    for x in 0..100 {
+        if x % 15 == 0 {
             println!("fizzbuzz")
-        } else if count % 3 == 0 {
+        } else if x % 3 == 0 {
             println!("fizz");
-        } else if count % 5 == 0 {
+        } else if x % 5 == 0 {
             println!("buzz");
         } else {
-            println!("{}", count)
+            println!("{}", x)
         }
-        count += 1
     }
 }
