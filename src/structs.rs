43b1eca80fa044af56b58bad2ca81fafbae3353c
Sudharsan Ravikumar <supersudh@gmail.com>
2024-04-28 11:40:32 +0100

    [Structs] Ability to grab two props from structs

--- src/structs.rs
+++ src/structs.rs
@@ -5,7 +5,22 @@
 // }
 
 // Tuple Struct
-struct Color(u8, u8, u8);
+// struct Color(u8, u8, u8);
+
+struct Person {
+    first_name: String,
+    last_name: String,
+}
+
+impl Person {
+    // Construct Person
+    fn new(first: &str, last: &str) -> Person {
+        Person {
+            first_name: first.to_string(),
+            last_name: last.to_string(),
+        }
+    }
+}
 
 pub fn run() {
     // let mut c = Color {
@@ -15,10 +30,12 @@ pub fn run() {
     // };
     // c.red = 200;
 
+    // let mut c = Color(255, 0, 0);
 
-    let mut c = Color(255, 0, 0);
+    // c.0 = 147;
 
-    c.0 = 147;
+    // println!("Color: {} {} {}", c.0, c.1, c.2);
 
-    println!("Color: {} {} {}", c.0, c.1, c.2);
+    let p = Person::new("Sudharsan", "Ravikumar");
+    println!("Person {} {}", p.first_name, p.last_name);
 }
